edc7aa18a6c213f5fab9c51903bbef2bff75ecea
vikash sharma <vikashtiwari9893@gmail.com>
2026-05-28 19:29:34 +0530

    code

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-ecodecide-hackathon-project-1.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
